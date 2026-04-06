edcd3215ff69659d9bdd7ae690355049e54ae8d9
abdukodir <abdukodirsheraliev1@gmail.com>
2026-04-06 22:10:26 +0900

    fix: modify bugs related files structure

--- .next-dev/static/chunks/webpack.js
+++ .next-dev/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0aa8c6d68f695e82"; }
+/******/ 		__webpack_require__.h = function() { return "2bd757b9ee9911c7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next-dev/static/webpack/webpack.821f658625154eb2.hot-update.js
+++ .next-dev/static/webpack/webpack.821f658625154eb2.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "585911425b48e232"; }
+/******/ 	__webpack_require__.h = function() { return "2bd757b9ee9911c7"; }
 /******/ }();
 /******/ 
 /******/ }
